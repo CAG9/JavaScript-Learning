83f74cf32b238c81abd366e21fabb07b44bf28c1
Cesar Arcos <60860385+CAG9@users.noreply.github.com>
2022-10-10 11:20:58 -0500

    Create variables.js

---
+++ Code/variables.js
@@ -0,0 +1,13 @@
+let variable = "freeCodeCamp";
+
+console.log(variable);
+
+// change value naem
+variable = 16;
+console.log(variable);
+variable = true;
+console.log(variable);
+variable = false;
+
+
+let myName = "Cesar";
